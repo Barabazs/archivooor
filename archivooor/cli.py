af729d33f9d4b0c7b4281eee6ecc1828365f17ca
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-06-07 13:36:27 +0200

    feat: replace argparse with click based CLI  (#16)
    * feat: add click dependency
    
    * feat: replace argparse with click based CLI

---
+++ archivooor/cli.py
@@ -0,0 +1,98 @@
+"""Command line interface for the archivooor package."""
+
+import click
+from dotenv import dotenv_values
+
+from archivooor.archiver import Archiver
+
+
+@click.group(
+    context_settings={
+        "help_option_names": ["-h", "--help"],
+        "ignore_unknown_options": True,
+    }
+)
+@click.version_option()
+@click.pass_context
+def cli(ctx):
+    """Easily interact with the archive.org API.
+
+    Submit webpages to the Wayback Machine and check the save job status.
+    """
+    settings = dotenv_values(".env")
+
+    archive = Archiver(settings.get("s3_access_key"), settings.get("s3_secret_key"))
+    ctx.obj = archive
+
+
+@cli.command(name="save")
+@click.argument("urls", nargs=-1)
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
+def save(urls, verbose):
+    """Save 1 or multiple URLS to the Wayback Machine.
+
+    Multiple URLs can be passed as space-separated arguments.
+
+    Example:
+
+    \b
+        $ archivooor save https://www.example.com https://www.example.org
+    """
+    if not urls:
+        click.echo(save.get_help(click.Context(save)))
+        return
+
+    responses = click.get_current_context().obj.save_pages(
+        pages=list(urls),
+        capture_all=True,
+        capture_outlinks=True,
+        force_get=True,
+        capture_screenshot=True,
+        skip_first_archive=True,
+        outlinks_availability=True,
+    )
+    if verbose:
+        for response in responses:
+            for key, value in response.items():
+                click.echo(f"{key}: {value}")
+    else:
+        for response in responses:
+            click.echo(
+                f"status: {response.get('status')}\njob_id: {response.get('job_id')}"
+            )
+
+
+@cli.command(name="job")
+@click.argument("job_id", nargs=1)
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
+def job(job_id, verbose):
+    """Get the status of a job by JOB_ID.
+
+    JOB_ID is the unique identifier of the job returned by the save command.
+    """
+    job_response = click.get_current_context().obj.get_save_status(job_id=job_id)
+    if verbose:
+        for key, value in job_response.items():
+            click.echo(f"{key}: {value}")
+    else:
+        click.echo(f"status: {job_response.get('status')}")
+        click.echo(f"original_url: {job_response.get('original_url')}")
+        click.echo(
+            f"outlinks_saved: {len(job_response.get('outlinks')) if \
+                job_response.get('outlinks') else 0}"
+        )
+
+
+@cli.command(name="stats")
+def stats():
+    """Get stats about your account.
+
+    The stats include the number of active and available sessions.
+    """
+    user_stats = click.get_current_context().obj.get_user_status_request()
+    for key, value in user_stats.items():
+        click.echo(f"{key}: {value}")
+
+
+if __name__ == "__main__":
+    cli()  # pylint: disable=no-value-for-parameter
